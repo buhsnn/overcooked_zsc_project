4ac0b1924da3160e19b91982bc2ced727802104d
Daehyun We <dwenny71513@gmail.com>
2025-12-10 17:18:38 +0900

    Clean cache, update overcooked_ai, update script

--- main_ours.sh
+++ main_ours.sh
@@ -1,2 +1,18 @@
-CUDA_VISIBLE_DEVICES=4 python main.py \
-    --train_steps_per_iter $1 \
+tspi=(
+    5000
+    10000
+    20000
+    40000
+)
+
+for tspi_val in "${tspi[@]}"; do
+    CUDA_VISIBLE_DEVICES=4 python main.py \
+        --train_steps_per_iter $tspi_val \
+        --w_novelty 0.01 &
+    CUDA_VISIBLE_DEVICES=5 python main.py \
+        --train_steps_per_iter $tspi_val \
+        --w_novelty 0.03 &
+    CUDA_VISIBLE_DEVICES=6 python main.py \
+        --train_steps_per_iter $tspi_val \
+        --w_novelty 0.1 &
+done
